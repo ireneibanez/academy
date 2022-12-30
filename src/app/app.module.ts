f8866a00e9a66cb8b7edcfbb0600d92883b76658
Irene Ibáñez Martínez <46047647+ireneibanez@users.noreply.github.com>
2022-12-30 01:53:26 +0100

    Advances in form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
 import { SharedModule } from './shared/shared.module';
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { CommonModule } from '@angular/common';
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,16 @@ import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
     HomeComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule, 
     AppRoutingModule,
     SharedModule,
-    GridModule, PagerModule,
-    ButtonModule 
+    GridModule, 
+    PagerModule,
+    ButtonModule,
+    DatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+// import { FormBuilder } from '@angular/forms';
+// import { Validators } from '@angular/forms';
+// import { FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormsModule, AbstractControl } from '@angular/forms';
+import { FormValidators } from '@syncfusion/ej2-angular-inputs';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  reactForm: FormGroup;
+
+  constructor() {
+    this.reactForm = new FormGroup({
+      'name': new FormControl('', [FormValidators.required]),
+      'surname': new FormControl('', [FormValidators.required]),
+      'email': new FormControl('', [FormValidators.email]),
+      'date': new FormControl('', [FormValidators.date])
+    });
+  }
+
+  ngOnInit(): void {
+    let formId: HTMLElement = <HTMLElement>document.getElementById('formId');
+    document.getElementById('formId')?.addEventListener(
+      'submit',
+      (e: Event) => {
+        e.preventDefault();
+        if (this.reactForm.valid) {
+          alert('Customer details added!');
+          this.reactForm.reset();
+        } else {
+          // validating whole form
+          Object.keys(this.reactForm.controls).forEach(field => {
+            const control = this.reactForm.get(field);
+            control?.markAsTouched({ onlySelf: true });
+          });
+        }
+      });
+  }
+
+  get name() { return this.reactForm.get('name'); }
+  get surname() { return this.reactForm.get('surname'); }
+  get email() { return this.reactForm.get('email'); }
+  get date() { return this.reactForm.get('date'); }
 
 }
